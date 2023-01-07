b555312e277377ac26382ad4d5f34818c473d350
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-07 15:16:21 +0600

    basic app setup with packages, ui and theme

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui:{
+    themes: [
+      {
+        arkTheme:{
+          primary: '#636365',
+          secondary: '#89939E',
+          accent: "#89939E",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+        }
+      }
+    ]
+  },
+  theme: {
+    fontFamily: {
+      custom1: ["Custom-1", "sans-serif"],
+      custom2: ["Custom-2", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
